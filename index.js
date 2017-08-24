674047eaa70bd53ab64464f23b4dea05e0b8da93
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-08-24 17:21:53 +0200

    created a route to greet a user

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
 const form = require('body-parser');
+
+
 var app = express();
 
-// seting rendering engine
+// setting rendering engine
 app.engine("hbs",exphbs({
   defaultLayout:"main",
   extname:"hbs"
@@ -18,9 +20,14 @@ app.set("view engine", "hbs")
 app.get("/",function(req,res) {
   res.render("home");
 });
+
 app.post("/",function(req,res){
-  res.send(req.body.name);
+  var massage = "Hello " + req.body.name;
+  res.render("home", {
+    display: massage
+  });
 });
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Server running at http://localhost:"+port+"/");
