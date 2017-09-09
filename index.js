0061b001d20b9ae6da0164b5f351776a4b07b86a
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-09-09 16:05:32 +0200

    add reset button and links for greeted users

--- index.js
+++ index.js
@@ -13,92 +13,126 @@ const models = Models(mongoURL);
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // parse application/json
 app.use(bodyParser.json());
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 * 30 }}));
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 60000 * 30
+    }
+}));
 app.use(flash());
 
 // setting rendering engine
-app.engine("hbs",exphbs({
-  defaultLayout:"main",
-  extname:"hbs"
+app.engine("hbs", exphbs({
+    defaultLayout: "main",
+    extname: "hbs"
 }));
 app.use(express.static("public"));
 app.use(express.static("views"))
 app.use(form.urlencoded({
-  extended:true
+    extended: true
 }));
 app.set("view engine", "hbs")
 
-app.get("/",function(req,res) {
-  res.render("home");
+app.get("/", function(req, res) {
+    res.render("home");
 });
 
 var namesGreeted = [];
-var index = [];
 
-app.post("/",function(req, res, next){
-
-  var greetedUser = req.body.name;
-  var language = req.body.language;
-  var greetMassage = "";
-
-  if (language === "English") {
-      greetMassage = "Hello " + greetedUser;
-  }else if (language === "Afrikaans") {
-      greetMassage = "Halo " + greetedUser;
-  }else if (language === "Xhosa") {
-      greetMassage = "Molo " + greetedUser;
-  }
-models.Users.create({
-  name: greetedUser
-}, function (err, person) {
-  if (err) {
-    if (err.code === 11000) {
-      req.flash("error", "Opps! The name already exist!");
-    }
-    res.redirect("/")
-    // return next(err)
-  }
-  else {
-    models.Users.find({}, function (err, results) {
-      if (err) {
-        return next(err);
-      }
-      else {
-        res.render("home", {
-          display: greetMassage,
-          counter: results.length
-        });
-        console.log(results.length);
-
-      }
+app.get("/greeted/:name", function(req, res) {
+    var name = req.params.name
+    models.Users.findOne({
+        name: name
+    }, function(error, results) {
+        if (error) {
+            console.log(error);
+        } else if (results) {
+            console.log("results");
+            res.render("timesGreeted", {
+                name: results
+            })
+        }
     })
-  }
+    //  return {makho};
 })
-  // namesGreeted.push(greetedUser);
+
+app.post("/", function(req, res, next) {
+
+    var greetedUser = req.body.name;
+    var language = req.body.language;
+    var greetMassage = "";
+
+    if (language === "English") {
+        greetMassage = "Hello " + greetedUser;
+    } else if (language === "Afrikaans") {
+        greetMassage = "Halo " + greetedUser;
+    } else if (language === "Xhosa") {
+        greetMassage = "Molo " + greetedUser;
+    }
+    models.Users.create({
+        name: greetedUser
+    }, function(err, person) {
+        if (err) {
+            if (err.code === 11000) {
+                req.flash("error", "Opps! The name already exist!");
+            }
+            res.redirect("/")
+            // return next(err)
+        } else {
+            models.Users.find({}, function(err, results) {
+                if (err) {
+                    return next(err);
+                } else {
+                    res.render("home", {
+                        display: greetMassage,
+                        counter: results.length
+                    });
+                    console.log(results.length);
+
+                }
+            })
+        }
+    })
+    // namesGreeted.push(greetedUser);
 });
 
 app.get('/greeted', function(req, res, next) {
-  // res.send(namesGreeted);
+    // res.send(namesGreeted);
 
-  models.Users.find({}, function(err, results) {
-   if (err) {
-     return next(err);
-   }
-   else {
-     res.render("greeted", {greeted: results});
+    models.Users.find({}, function(err, results) {
+        if (err) {
+            return next(err);
+        } else {
+            res.render("greeted", {
+                greeted: results
+            });
 
-   }
+        }
 
-  console.log(namesGreeted);
-  });
+        // console.log(namesGreeted);
+    });
+});
+
+app.post('/reset', function(req, res) {
+    models.Users.remove({}, function(error, results) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("data");
+        }
+        res.render("home");
+    })
+    //  return {lucky};
 });
 
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Server running at http://localhost:"+port+"/");
+    console.log("Server running at http://localhost:" + port + "/");
 });
